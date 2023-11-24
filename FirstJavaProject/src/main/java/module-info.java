9dc52d88dd3cd358b27125f5d149fc0570514afb
Christian Guidikov <90891586+ChristianGuidikov@users.noreply.github.com>
2023-11-24 21:18:54 +0200

    Delete FirstJavaProject directory

--- FirstJavaProject/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.firstjavaproject {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.firstjavaproject to javafx.fxml;
-    exports com.example.firstjavaproject;
-}
